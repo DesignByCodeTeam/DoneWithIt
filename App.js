2f4bc7377ce5970b12453fb9afd0a8d6f038536c
Claude Myburgh <claudemyburgh@gmail.com>
2020-10-31 22:11:50 +0200

    First commit

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useDimensions} from "@react-native-community/hooks";
+
 
 export default function App() {
+  const {screen: {width, height}} = useDimensions()
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
